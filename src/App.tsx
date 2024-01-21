80ec2e0ad93d12f74324dda7d89957977f61e285
dkilfoyle <dkilfoyle@xtra.co.nz>
2024-01-22 10:53:32 +1300

    first

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import { useEffect, useState } from "react";
+import { ChakraProvider, Divider, Flex, Heading, VStack } from "@chakra-ui/react";
+import { CodeEditor } from "./CodeEditor";
+import { registerLanguages } from "./languages/loader";
+registerLanguages();
+
+export default function App() {
+  const [text, setText] = useState('print("Hello world!")');
+  return (
+    <ChakraProvider>
+      <Flex h="100vh" w="100vw" p={10}>
+        <VStack w="100%" boxShadow={"md"} p={4} borderStyle={"solid"} borderWidth={1} rounded={"lg"}>
+          <Heading>Code Editor</Heading>
+          <Divider />
+          <CodeEditor></CodeEditor>
+        </VStack>
+      </Flex>
+    </ChakraProvider>
+  );
+}
